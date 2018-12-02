d1d504c71df106c552ec322fff9da7aa27e65f63
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2018-12-02 13:15:23 +0700

    drivers and creating db

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"regexp"
 
@@ -8,10 +9,54 @@ import (
 )
 
 const (
-	host = "localhost"
-	port = 5432
-	user = 
+	host     = "localhost"
+	port     = 5432
+	user     = "cocowork"
+	password = "rahasia"
+	dbName   = "gophercises_phone"
 )
+
+func main() {
+	// reset db first
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
+	db, err := sql.Open("postgres", psqlInfo)
+	// db.Close()
+	must(err)
+	err = resetDB(db, dbName)
+	must(err)
+
+	// connect to new database that has been crated
+	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbName)
+	db, err = sql.Open("postgres", psqlInfo)
+	must(err)
+	defer db.Close()
+	must(db.Ping())
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+}
+
+func createDB(db *sql.DB, name string) error {
+	_, err := db.Exec("CREATE DATABASE " + name)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Successfully create db")
+	return nil
+}
+
+func resetDB(db *sql.DB, name string) error {
+	_, err := db.Exec("DROP DATABASE IF EXISTS " + name)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Successfully drop db")
+	return createDB(db, dbName)
+}
 
 // func normalize(phone string) string {
 // 	var buf bytes.Buffer
@@ -37,9 +82,4 @@ func normalize(phone string) string {
 
 	// replace non digit with ""
 	return re.ReplaceAllString(phone, "")
-}
-
-func main() {
-	res := normalize("12345678")
-	fmt.Println(res)
 }
